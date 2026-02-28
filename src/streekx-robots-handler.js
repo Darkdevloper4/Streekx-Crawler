be7687e95188ac700b90e3c39305d75e7e40ee91
Darkdevloper4 <chanansh720@gmail.com>
2026-02-28 20:28:50 +0530

    Implement URL access check using robots.txt
    Add isAllowed function to check URL access based on robots.txt

--- src/streekx-robots-handler.js
+++ src/streekx-robots-handler.js
@@ -1,1 +1,17 @@
+const axios = require('axios');
+const robotsParser = require('robots-parser');
+
+async function isAllowed(url) {
+    try {
+        const urlObj = new URL(url);
+        const robotsUrl = `${urlObj.origin}/robots.txt`;
+        const response = await axios.get(robotsUrl, { timeout: 5000 });
+        const robots = robotsParser(robotsUrl, response.data);
+        return robots.isAllowed(url, 'StreekxBot');
+    } catch (e) {
+        return true; // Agar robots.txt nahi hai toh allow karein
+    }
+}
+
+module.exports = { isAllowed };
 
